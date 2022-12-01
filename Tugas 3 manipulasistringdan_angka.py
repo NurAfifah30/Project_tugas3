b5e211cb7184b31c291537c84b499798944e3eba
NurAfifah30 <115690241+NurAfifah30@users.noreply.github.com>
2022-12-01 09:28:24 +0700

    manipulasistringdan_angka

---
+++ Tugas 3 manipulasistringdan_angka.py
@@ -0,0 +1,12 @@
+jumlah = 3
+barang = 567
+harga = 49.95
+myorder = "SAYA MEMBELI {} dus ,sejumlah barang {} dengan harga {} rupiah"
+print(myorder.format(jumlah,barang, harga))
+
+
+jumlah = 3
+barang = 567
+harga = 49.95
+myorder = "SAYA MEMBELI {2} RUPIH PISANG SEBANYA {0} dus sebanya {1} barang."
+print(myorder.format(jumlah,barang, harga))
